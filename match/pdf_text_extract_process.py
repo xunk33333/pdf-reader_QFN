4588050d454696c90e8edb8ff6466a5f903047e4
xunke <1319978370@qq.com>
2023-04-18 12:33:44 +0800

    去除框内的多余的text

--- match/pdf_text_extract_process.py
+++ match/pdf_text_extract_process.py
@@ -1,13 +1,15 @@
+from decimal import Decimal
 import os
 import pickle
 import re
 import shutil
+import matplotlib.path as mplPath
+import numpy as np
 
 
-def match_text_num(num_data,text_data, threshold=7):
+def match_text_num(num_data, text_data, threshold=7):
     result = []
 
-    
     for center_x, center_y, text in num_data:
         distance_x_list = [(abs(x[1] - center_y),
                             x[2]
@@ -22,22 +24,24 @@ def match_text_num(num_data,text_data, threshold=7):
 
 
 def text_filter(txt):
-    
+
     var = not (re.search("QFN", txt, re.IGNORECASE)
                or re.search("view", txt, re.IGNORECASE)
                or re.search("top", txt, re.IGNORECASE)
                or re.search("Thermal", txt, re.IGNORECASE)
                or re.search("pad", txt, re.IGNORECASE)
                )
-    
 
     return var
+
+
 def successive_digit(txt):
-    words = [x for x in txt.split(' ') if not x=='']
+    words = [x for x in txt.split(' ') if not x == '']
     for word in words:
         if not word.isdigit():
             return False
     return True
+
 
 def concat_span_text(line_spans):
     if line_spans.__len__() == 1:
@@ -48,6 +52,7 @@ def concat_span_text(line_spans):
             text = text + span['text']
         return text
 
+
 def split_span_text(lines):
     lines_copy = lines.copy()
     for line in lines_copy:
@@ -56,7 +61,7 @@ def split_span_text(lines):
             lines.remove(line)
             span = spans[0]
             words = span['text'].split(' ')
-            l,t,r,b = span['bbox']
+            l, t, r, b = span['bbox']
             w = r - l
             h = b - t
             sum_len = len(span['text'])
@@ -64,29 +69,26 @@ def split_span_text(lines):
             s_w = w/sum_len
             y_center = t + h/2
             dis_num = 0
-            for i,word in enumerate(words):
+            for i, word in enumerate(words):
                 left = l + s_w*(dis_num+i)
-                dis_num += len(word) 
+                dis_num += len(word)
                 right = left + s_w*len(word)
                 x_center = left/2 + right/2
                 lines.append({'text': word,
-                         'origin': (x_center,y_center),
-                         'spans':[
-                                {
-                                    "size":span['size'],
-                                    'origin':(x_center,y_center)
-                                }
-                         ],
-                         'dir':line['dir']
-                         })
-            
+                              'origin': (x_center, y_center),
+                              'spans': [
+                                  {
+                                      "size": span['size'],
+                                      'origin':(x_center, y_center)
+                                  }
+                              ],
+                              'dir': line['dir']
+                              })
 
-            
 
 def concat_line_text(lines):
-    
 
-    #拼接同一个line不同span中的字符
+    # 拼接同一个line不同span中的字符
     lines_copy = lines.copy()
     for line in lines_copy:
         try:
@@ -94,22 +96,23 @@ def concat_line_text(lines):
             if line['spans'].__len__() == 0 or not text_filter(line['text']):
                 lines.remove(line)
         except:
-            continue#由于split span后新加入的line不规范，没有，key：spans所以跳过
-    
-    #拼接下标
+            continue  # 由于split span后新加入的line不规范，没有，key：spans所以跳过
+
+    # 拼接下标
     concated_lines = []
     # 1 比较字体大小，找到下标字体.
     standard_font_size = lines[0]['spans'][0]['size']
     lines_copy = lines.copy()
     for line in lines_copy:
         try:
-            if standard_font_size - line['spans'][0]['size'] > 1:  # 2 根据角度dir，区分拼接顺序与筛选条件：5与50
-            #if True:
+            # 2 根据角度dir，区分拼接顺序与筛选条件：5与50
+            if standard_font_size - line['spans'][0]['size'] > 1:
+                # if True:
                 concated_line = concat_subscript_text(lines, line)
                 if concated_line is not None:
                     concated_lines.append(concated_line)
         except:
-            continue#由于split span后新加入的line不规范，没有，key：spans所以跳过
+            continue  # 由于split span后新加入的line不规范，没有，key：spans所以跳过
     lines += concated_lines
 
 
@@ -123,7 +126,8 @@ def concat_subscript_text(lines, subline, threshold1=5, threshold2=50):
                 continue
             elif abs(line['spans'][0]['origin'][1] - subline['spans'][0]['origin'][1]) < threshold1 and abs(
                     line['spans'][0]['origin'][0] - subline['spans'][0]['origin'][0]) < threshold2:
-                potential_match.append((line['spans'][0]['origin'], line['text']))
+                potential_match.append(
+                    (line['spans'][0]['origin'], line['text']))
                 lines.remove(line)
 
         text = ''
@@ -140,8 +144,6 @@ def concat_subscript_text(lines, subline, threshold1=5, threshold2=50):
         concated_line = {'text': text,
                          'origin': (origin_x / potential_match.__len__(), origin_y / potential_match.__len__())}
         return concated_line
-
-
 
     elif abs(subline['dir'][0]) < 0.1 and subline['dir'][0] > 0:  # 下到上拼接
         pass
@@ -160,10 +162,10 @@ def get_original_data(page, clip):
         #     concat_line_flag = False
         lines = lines + block['lines']
 
-    #处理span拆成多个line，主要针对数字
+    # 处理span拆成多个line，主要针对数字
     # split_span_text(lines) #拆开后会导致line信息丢失，暂时关闭
 
-    #连接line line的text
+    # 连接line line的text
     concat_line_text(lines)
 
     for line in lines:
@@ -177,7 +179,7 @@ def get_original_data(page, clip):
                         line['origin'][1],
                         line['text'])
                         )
-    
+
     for center_x, center_y, text in data:
         if text.isdigit():
             num_data.append((center_x, center_y, text))
@@ -186,26 +188,90 @@ def get_original_data(page, clip):
         else:
             text_data.append((center_x, center_y, text))
 
-    #上面获取num——data有问题，所以用下面方法补充
+    # 上面获取num——data有问题，所以用下面方法补充
     words = page.get_text("words", clip=clip)
-    for _,_,right,bottom,txt,block_no,line_no,word_no in words:
+    for _, _, right, bottom, txt, block_no, line_no, word_no in words:
         if txt.isdigit() and txt not in [x[2] for x in num_data]:
             num_data.append((right-2, bottom-2, txt))
 
-    #数字 字符在一个span中的特殊情况处理
+    # 数字 字符在一个span中的特殊情况处理
     def synthetic_num_txt(text):
-        words = [x for x in text.split(' ') if not x=='']
-        if words.__len__()==2:
+        words = [x for x in text.split(' ') if not x == '']
+        if words.__len__() == 2:
             if words[0].isdigit() and not words[1].isdigit():
                 return words[1]
             elif words[1].isdigit() and not words[0].isdigit():
                 return words[0]
         else:
             return text
-    text_data = [(center_x, center_y,synthetic_num_txt(text))
-                  for center_x, center_y, text in text_data ]
-    
-    return num_data,text_data
+    text_data = [(center_x, center_y, synthetic_num_txt(text))
+                 for center_x, center_y, text in text_data]
+
+    # 给数字排个序
+    num_data = [(x, y, int(txt)) for x, y, txt in num_data]
+    num_data = sorted(num_data, key=lambda x: int(x[2]))
+    if num_data[-1][2] % 2 == 1:  # 删除奇数
+        num_data.pop(-1)
+
+    # 去除框内的多余的text
+
+    def get_gravity_point(points):
+        """
+        @brief      获取多边形的重心点
+        @param      points  The points
+        @return     The center of gravity point.
+        """
+        if len(points) <= 2:
+            return list()
+
+        area = Decimal(0.0)
+        x, y = Decimal(0.0), Decimal(0.0)
+        for i in range(len(points)):
+            lng = Decimal(points[i][0])
+            lat = Decimal(points[i][1])
+            nextlng = Decimal(points[i-1][0])
+            nextlat = Decimal(points[i-1][1])
+
+            tmp_area = (nextlng*lat - nextlat*lng)/Decimal(2.0)
+            area += tmp_area
+            x += tmp_area*(lng+nextlng)/Decimal(3.0)
+            y += tmp_area*(lat+nextlat)/Decimal(3.0)
+        x = x/area
+        y = y/area
+        return [float(x), float(y)]
+
+    def create_equal_ratio_points(points, ratio, gravity_point):
+        """
+        @brief      创建等比例的点
+        @param      points         The points
+        @param      ratio          The ratio
+        @param      gravity_point  The gravity point
+        @return     { description_of_the_return_value }
+        """
+        # 判断输入条件
+        if len(points) <= 2 or not gravity_point:
+            return list()
+
+        new_points = list()
+        length = len(points)
+
+        for i in range(length):
+            vector_x = points[i][0] - gravity_point[0]
+            vector_y = points[i][1] - gravity_point[1]
+            new_point_x = ratio * vector_x + gravity_point[0]
+            new_point_y = ratio * vector_y + gravity_point[1]
+            new_point = [new_point_x, new_point_y]
+            new_points.append(new_point)
+        return new_points
+
+    num_points = [[x, y] for x, y, _ in num_data]
+    num_points = create_equal_ratio_points(
+        num_points, 0.8, get_gravity_point(num_points))
+    poly_path = mplPath.Path(np.array(num_points))
+    text_data = [(center_x, center_y, text) for center_x, center_y, text in text_data
+                 if not poly_path.contains_point((center_x, center_y))]
+
+    return num_data, text_data
 
 
 def del_dir(path):
@@ -221,4 +287,3 @@ def del_dir(path):
             shutil.rmtree(filepath, True)  # 若为文件夹，则删除该文件夹及文件夹内所有文件
             # print("dir "+str(filepath)+" removed!")
     shutil.rmtree(rootdir, True)  # 最后删除img总文件夹
-
